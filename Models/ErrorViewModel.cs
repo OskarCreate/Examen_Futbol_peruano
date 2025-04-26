71b93b84bdd0101bfa61efb0a02ad34ae701ab7f
OskarCreate <oskar_morales_f@outlook.com>
2025-04-25 20:30:31 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Examen_Futbol_peruano.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
